d5963c2e314a04308eaaee5a5092505a092f3940
unknown <otavio.paiva93@yahoo.com.br>
2020-09-09 18:26:28 -0300

    add PUT and DELETE

--- teachers.js
+++ teachers.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const data = require('./data.json')
-const { age, date, graduation, selectGraduation} = require('./utils')
+const { age, date, graduation} = require('./utils')
 
 // create
 exports.post = function (req,res){
@@ -17,7 +17,7 @@ exports.post = function (req,res){
 
     birth = Date.parse(birth)
     const created_at = Date.now()
-    const id = Number(data.teachers.length + 1)    
+    const id = Number(data.teachers.length + 1)   
 
     data.teachers.push({
         id,
@@ -70,8 +70,58 @@ exports.edit = function(req, res){
 
     const teacher = {
         ...foundTeacher,
-        birth: date(foundTeacher.birth)
+        birth: date(foundTeacher.birth),
+        
     }    
     
-    return res.render('teachers/edit', { teacher, selectGraduation })
+    return res.render('teachers/edit', { teacher })
 }
+
+exports.put = function(req, res){
+
+    const { id } = req.body
+    let index = 0
+
+    const foundTeacher = data.teachers.find(function(teacher, foundIndex){
+        if(id == teacher.id) {
+           index = foundIndex
+           return true
+    }
+})
+
+if (!foundTeacher) return res.send("Teacher not found!")
+
+const teacher = {
+    ...foundTeacher,
+    ...req.body,
+    birth: Date.parse(req.body.birth)
+}
+
+data.teachers[index] = teacher
+
+fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+    if(err) return res.send("Write error!")
+
+    return res.redirect(`/teachers/${id}`)
+})
+
+}
+
+exports.delete = function(req, res){
+    const { id } = req.body
+
+    const filteredTeachers = data.teachers.filter(function(teacher){
+        return teacher.id != id
+    })
+
+    data.teachers = filteredTeachers
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err){
+        if (err) return res.send("Write file error!")
+
+        return res.redirect("/teachers")
+
+    })
+}
+
+
